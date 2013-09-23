b93d48a24d86a0fcae169c7a751bccc6e06190a6
Nephi Wright <nephiw@acm.org>
2013-09-22 19:50:23 -0600

    server now starts and stops when requested.

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -3,3 +3,56 @@
  * Date: 9/22/13
  * Time: 5:47 PM
  */
+
+var ROOT_URL = 'http://localhost:3009/',
+    http = require('http'),
+    Server = require('./server.js'),
+    server;
+
+exports.setUp = function(done) {
+    server = new Server();
+    done();
+};
+
+exports.tearDown = function (done) {
+    server = null;
+    done();
+};
+
+exports['A request for / returns 200 status code.'] = function (test) {
+    server.start();
+    httpGet(ROOT_URL, function (res) {
+        test.equal(200, res.statusCode, '/ failed to return with a status code of 200.');
+        server.stop(test.done);
+    });
+};
+
+exports['A request for index.html returns 200 status code.'] = function (test) {
+    server.start();
+    httpGet(ROOT_URL + 'index.html', function (res) {
+        test.equal(200, res.statusCode, 'index.html failed to return with a status code of 200');
+        server.stop(test.done);
+    });
+};
+
+exports['Attempting to stop the server when it is not running throws exception.'] = function (test) {
+    test.throws(function () {
+        server.stop();
+    });
+    test.done();
+};
+
+function httpGet(url, callback) {
+    var request;
+    request = http.get(url);
+    request.on('response', function (response) {
+        var receivedData = '';
+        response.setEncoding('utf8');
+        response.on('data', function (chunk) {
+            receivedData += chunk;
+        });
+        response.on('end', function () {
+            callback(response, receivedData);
+        });
+    });
+}
--- src/server/server.js
+++ src/server/server.js
@@ -3,3 +3,37 @@
  * Date: 9/22/13
  * Time: 5:47 PM
  */
+
+var express = require('express'),
+    expServer, runningServer;
+
+function handleHomeRequest(req, res) {
+    var body = '<html><body>Home Page.</body></html>';
+    res.send(body);
+}
+
+function Server() {
+    var _this = this;
+    this.isRunning = false;
+}
+
+Server.prototype.start = function () {
+    expServer = express();
+
+    expServer.get('/', function (req, res) {
+        handleHomeRequest(req, res);
+    });
+    expServer.get('/index.html', function (req, res) {
+        handleHomeRequest(req, res);
+    });
+
+    this.isRunning = true;
+    runningServer = expServer.listen(3009);
+};
+
+Server.prototype.stop = function(cb) {
+    this.isRunning = false;
+    runningServer.close(cb);
+};
+
+module.exports = Server;
